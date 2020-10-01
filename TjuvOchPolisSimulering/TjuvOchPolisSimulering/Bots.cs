14ce54dd1cea7a688bbdd5e0c86aa38e719badef
dderiba <55302543+dderiba@users.noreply.github.com>
2020-10-02 00:04:32 +0200

    Oklar

---
+++ TjuvOchPolisSimulering/TjuvOchPolisSimulering/Bots.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TjuvOchPolisSimulering
+{
+    abstract class Bot
+    {
+        public int PosX;
+        public int PosY;
+        public int lastPosX;
+        public int lastPosY;
+        public int Type;
+
+        // Tilldelar en random x-koordinat till objekt (0-100).
+        public void CoordX()
+        {
+            Random rng1 = new Random();
+            int xRan = rng1.Next(0, 100);
+
+            PosX = xRan;
+        }
+
+        // Tilldelar en random y-koordinat till objekt (0-25).
+        public void CoordY()
+        {
+            Random rng2 = new Random();
+            int yRan = rng2.Next(0, 25);
+
+            PosY = yRan;
+        }
+
+        public List<string> inventoryList = new List<string>();
+
+        abstract public void Body();
+        abstract public void BotType();
+    }
+
+    class Citizen : Bot
+    {
+
+        public void AddItems()
+        {
+            inventoryList.Add("Nycklar");
+            inventoryList.Add("Mobiltelefon");
+            inventoryList.Add("Pengar");
+            inventoryList.Add("Klocka");
+        }
+        public override void BotType()
+        {
+            Type = 1;
+        }
+        public override void Body()
+        {
+            Console.Write("M");
+        }
+    }
+
+    class Thief : Bot
+    {
+        public override void BotType()
+        {
+            Type = 2;
+        }
+
+        public override void Body()
+        {
+            Console.Write("T");
+        }
+    }
+
+    class Police : Bot
+    {
+        public override void BotType()
+        {
+            Type = 3;
+        }
+
+        public override void Body()
+        {
+            Console.Write("P");
+        }
+    }
+
+    class Position : Bot
+    {
+        public int X;
+        public int Y;
+        public int PosType;
+
+        public override void Body()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void BotType()
+        {
+            throw new NotImplementedException();
+        }
+    }   
+}
---
+++ TjuvOchPolisSimulering/TjuvOchPolisSimulering/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using static System.Console;
+namespace TjuvOchPolisSimulering
+{
+    class Program
+    {
+        public static List<Bot> city = new List<Bot>();   
+        
+        static void Main(string[] args)
+        {
+            Settings();          // Ställer in inställningar för consolefönstret.
+            AddPopulation();     // Lägger till "befolkningen" till världen.
+            DrawWorld();         // Ritar ut världen.
+        }
+
+        private static void Settings()
+        {
+            Console.CursorVisible = false;
+            BufferWidth = 200;
+            BufferHeight = 200;
+            SetBufferSize(Console.BufferWidth, Console.BufferHeight);
+        }
+
+        private static void AddPopulation()
+        {
+            // Lägger till medborgare till staden.
+            for (int i = 0; i < 15; i++)
+            {
+                Citizen citizen = new Citizen();
+
+                citizen.CoordX();
+                citizen.CoordY();
+                citizen.BotType();
+                citizen.AddItems();
+                city.Add(citizen);
+            }
+
+            // Lägger till tjuvar till staden.
+            for (int i = 0; i < 10; i++)
+            {
+                Thief thief = new Thief();
+
+                thief.CoordX();
+                thief.CoordY();
+                thief.BotType();
+                city.Add(thief);
+            }
+
+            // Lägger till poliser till staden.
+            for (int i = 0; i < 7; i++)
+            {
+                Police police = new Police();
+
+                police.CoordX();
+                police.CoordY();
+                police.BotType();
+                city.Add(police);
+            }
+        }
+
+        // Om en karaktär förflyttar sig utanför kartans gräns: Karaktären transporteras istället till andra änden av X- respektive Y-axeln.
+        private static void TransportPosition()
+        {
+            foreach (Bot bot in city)
+            {
+                if (bot.PosX < 0)
+                {
+                    bot.PosX = 100;
+                }
+                if (bot.PosY < 0)
+                {
+                    bot.PosY = 25;
+                }
+                if (bot.PosX > 100)
+                {
+                    bot.PosX = 0;
+                }
+                if (bot.PosY > 25)
+                {
+                    bot.PosY = 0;
+                }
+            }
+        }
+
+        // Ger ett värde mellan 0-8 som matas in i DrawWorld() switchen, vilket i sin tur motsvarar en riktning (stop, ⇑, ⇗, ⇒, ⇘, ⇓, ⇙, ⇐, ⇖) som karaktären rör sig åt. 
+        private static int Direction()
+        {
+
+            Random rngDir = new Random();
+            int directionValue = rngDir.Next(0, 8);
+
+            return directionValue;
+        }
+        
+        // Ritar ut världen.
+        private static void DrawWorld()
+        {
+            while (true)
+            {               
+                Clear();
+                foreach (Bot bot in city)
+                {
+                    switch (Direction())
+                    {
+                        case 0:
+                           break;
+
+                        case 1:
+                            bot.PosY--;
+                            break;
+
+                        case 2:
+                            bot.PosX++;
+                            bot.PosY--;
+                            break;
+
+                        case 3:
+                            bot.PosX++;
+                            break;
+
+                        case 4:
+                            bot.PosX++;
+                            bot.PosY++;
+                            break;
+
+                        case 5:
+                            bot.PosY++;
+                            break;
+                        case 6:
+                            bot.PosX--;
+                            bot.PosY++;                         
+                            break;
+
+                        case 7:
+                            bot.PosX--;                            
+                            break;
+
+                        case 8:
+                            bot.PosX--;
+                            bot.PosY--;                         
+                            break;
+
+                        default:
+                            break;
+                    }
+                    TransportPosition();
+                    SetCursorPosition(bot.PosX, bot.PosY);
+                    bot.Body();                                                        
+                }               
+                Thread.Sleep(500);
+            }
+        }           
+    }
+}
